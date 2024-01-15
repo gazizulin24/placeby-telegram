2d7e6c8be5a43724bf4ad9f5df848f03951f6482
Timur Gazizulin <77000830+gazizulin24@users.noreply.github.com>
2024-01-15 11:18:16 +0300

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-TOKEN="6357160199:AAE_ydUwZncbWetB1i-9fbiCjwmcwPHd8Ps"
-CLOUD_NAME="dodfq0tck"
-API_KEY="412429297941325"
-API_SECRET="GP_qbgtOB09P2ubuisa5LE-6lbM"
-hello_words = "Админ команды\n\n| Добавить новую локацию: /locationnew\n\n| Получить файл с локациями: /locationsql\n\n| Получить json со всеми локациями: /locationjson"
-SUPER_ADMINS = ["714540502", "1091269484"]
-super_admin_menu = "Супер админ команды:\n\n| Запустить heroku сервер: /runheroku\n\n| Выключить heroku сервер: /stopheroku\n\n| Подключить heroku бд: /startdb\n\n| Выключить heroku бд: /stopdb"
